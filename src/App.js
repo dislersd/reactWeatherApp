446bab924443ae22eaf47923fd57f149f81fb3e5
dislersd <dislersd@gmail.com>
2018-12-26 14:59:50 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/form";
+import Weather from "./components/Weather";
 
-class App extends Component {
+const API_KEY = "38137445bf638664cef728f642857ddd";
+
+
+class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data); 
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
+      
     );
   }
-}
+};
 
 export default App;
